b59319ec16306f706799d45f7269742d5cf7111c
CKPhong <thansauzzz@gmail.com>
2021-05-03 18:10:25 +0700

    Cập nhập file cài đặt tuần tự và thiết kế phần 2
    File cài đặt tuần tự:
    - Cập nhật hàm grayscale và hàm tính SAT
    Thiết kế phần 2:
    - Cập nhật một số bước vào thiết kế

--- sequential.py
+++ sequential.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 from numba import jit
-# import ...
+import cv2
 
 @jit(nopython=True)
 def convert_rgb2gray(in_pixels, out_pixels):
@@ -16,8 +16,12 @@ def convert_rgb2gray(in_pixels, out_pixels):
     out_pixels : numpy.ndarray with shape=(h, w)
         Output image in grayscale
     '''
-
-    raise NotImplementedError()
+    for r in range(len(in_pixels)):
+        for c in range(len(in_pixels[r])):
+            blue = in_pixels[r][c][0]
+            green = in_pixels[r][c][1]
+            red = in_pixels[r][c][2]
+            out_pixels[r][c] = 0.114*blue + 0.587*green + 0.299*red
 
 
 @jit(nopython=True)
@@ -32,8 +36,19 @@ def calculate_sat(in_pixels, sat):
     sat : numpy.ndarray with shape=(h, w)
         Summed Area Table of input image
     '''
-
-    raise NotImplementedError()
+    height = len(in_pixels)
+    width = len(in_pixels[0])
+    for r in range(height):
+        for c in range(width):
+            x_left = c - 1
+            y_above = r - 1
+            sat[r][c] += in_pixels[r][c]
+            if x_left > -1 and x_left < width:
+                sat[r][c] += sat[r][x_left]
+                if y_above > -1 and y_above < height:
+                    sat[r][c] += sat[y_above][c] - sat[y_above][x_left]
+            elif y_above > -1 and y_above < height:
+                sat[r][c] += sat[y_above][c]
 
 
 def main():
@@ -45,33 +60,29 @@ def main():
     ofname = sys.argv[2]
 
     # Read image
-    raise NotImplementedError()
-    # img = ...
+    im = cv2.imread(ifname)
 
     # Convert image to grayscale using jitted function
-    gray_img = np.empty((img.shape[0], img.shape[1]), dtype=img.dtype)
-    convert_rgb2gray(img, gray_img)
+    gray_img = np.empty((im.shape[0], im.shape[1]), dtype=im.dtype)
+    convert_rgb2gray(im, gray_img)
 
     # Convert image to grayscale using Numpy function/operator
-    raise NotImplementedError()
-    # gray_img_np = ...
-    # ...
-
+    rgb_weights = [0.114, 0.587, 0.299]
+    gray_img_np = np.dot(im[...,:3], rgb_weights)
+    
     # Test convert_rgb2gray
     print('Jitted vs Numpy error:', 
           np.mean(np.abs(gray_img.astype(np.int16) - gray_img_np)))
     print('Jitted vs Opencv error:', 
           np.mean(np.abs(gray_img.astype(np.int16) - 
-                         cv.cvtColor(img, code=cv.COLOR_RGB2GRAY))))
+                         cv2.cvtColor(im, code=cv2.COLOR_BGR2GRAY))))
 
     # Calculate summed area table using jitted function
     sat = np.empty(gray_img.shape, dtype=np.int64)
     calculate_sat(gray_img, sat)
 
     # Calculate summed area table using Numpy function/operator
-    raise NotImplementedError()
-    # sat_np = ...
-    # ...
+    sat_np = gray_img.cumsum(axis=0).cumsum(axis=1)
 
     # Test
     assert(np.sum(gray_img) == sat[-1, -1])
@@ -79,8 +90,7 @@ def main():
     assert(np.array_equal(sat, sat_np))
 
     # Write image
-    raise NotImplementedError()
-
+    cv2.imwrite(ofname, gray_img)
 
 # Execute
 main()
